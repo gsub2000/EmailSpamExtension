8cea685ae0a54f3e63e476ffa06f9dc77a24c119
Gayatri Subramaniam <gayatrs@uci.edu>
2021-05-15 09:36:36 -0700

    Testing data format for pushing to backend

--- main.py
+++ main.py
@@ -47,6 +47,11 @@ def mergeEmails(selectedEmail, emails):
 
 @app.route('/data', methods=["POST"])
 def example():
+
+    print(request.get_json(force=True))
+    print(jsonify(request.get_json(force=True)))
+
+    
     email_data = request.get_json(force=True)['msg']
 
     emails = []
